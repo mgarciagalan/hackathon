82871f723d1161b094d9dafb1043f1d79bdb1801
Mariano García <mgarciagalan3@gmail.com>
2021-11-07 09:35:29 +0100

    [ADD] Checkout Styles

--- app/pages/checkout/partials/checkout-header.jsx
+++ app/pages/checkout/partials/checkout-header.jsx
@@ -5,23 +5,33 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React from 'react'
-import {FormattedMessage} from 'react-intl'
-import {Badge, Box, Button, Flex, Center} from '@chakra-ui/react'
+import {FormattedMessage, FormattedNumber} from 'react-intl'
+import {useIntl} from 'react-intl'
+import {Badge, Box, Button, Flex, Center, IconButton, useMultiStyleConfig} from '@chakra-ui/react'
 import useBasket from '../../../commerce-api/hooks/useBasket'
 import Link from '../../../components/link'
 import {BasketIcon, BrandLogo} from '../../../components/icons'
+import {LogoElMontaoMin} from '../../../components/custom-icons'
+
 
 const CheckoutHeader = () => {
     const basket = useBasket()
+    const intl = useIntl()
+    const styles = useMultiStyleConfig('Header')
 
     return (
         <Box px={[4, 4, 8]} bg="white" borderBottom="1px" borderColor="gray.100">
             <Box maxWidth="container.xxxl" marginLeft="auto" marginRight="auto">
                 <Flex h={{base: '52px', md: '80px'}} align="center" justify="space-between">
                     <Link href="/" title="Back to homepage">
-                        <BrandLogo
-                            width={{base: '35px', md: '45px'}}
-                            height={{base: '24px', md: '32px'}}
+                        <IconButton
+                        aria-label={intl.formatMessage({
+                            id: 'header.button.assistive_msg.logo',
+                            defaultMessage: 'Logo',
+                        })}
+                        icon={<LogoElMontaoMin {...styles.logo} />}
+                        {...styles.logoBtn}
+                        variant="unstyled"
                         />
                     </Link>
 
